0ab6caa83d034c181b238b0f895eabc3e4f8f446
jaiswalabhishek377 <jaiswalabhishek377@gmail.com>
2026-04-24 13:40:09 +0530

    Todo

---
+++ Phase-3/TodoApp/src/context/TodoContext.jsx
@@ -0,0 +1,22 @@
+import { createContext,useContext } from "react";
+
+// export const TodoContext = createContext();
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Learn React",
+            isCompleted: false,
+        },
+    ],
+    addTodo: () => {},
+    updatedTodo: () => {},
+    deleteTodo: () => {},
+    toggleComplete: () => {},
+});
+
+export const useTodo = () =>{
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
