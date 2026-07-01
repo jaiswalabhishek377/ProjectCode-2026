5d1ae32df8ee78baed8573733b52e934a71f208c
jaiswalabhishek377 <jaiswalabhishek377@gmail.com>
2026-07-01 23:57:33 +0530

    JWTAuth

---
+++ Phase-3/Authentication/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AuthPage from './pages/authpage'
+
+const App = () => {
+  return (
+    <>
+      <div className='App'>
+        <p>This is the App component</p>
+        <br/>
+        <AuthPage/>
+      </div>
+    </>
+    
+  )
+}
+
+export default App
